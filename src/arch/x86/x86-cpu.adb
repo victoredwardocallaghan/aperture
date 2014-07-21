c28e3fbcce53cb6dff9d33c13a3a952198a3b2db
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-07-22 03:51:48 +1000

    arch/x86/x86-cpu.ad*: Provide x86 CPU register procedures
    Provide a low-level API to access important 'x86' registers.
    
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

---
+++ src/arch/x86/x86-cpu.adb
@@ -0,0 +1,156 @@
+--
+-- Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
+--
+
+with System.Machine_Code;
+
+package body X86.CPU is
+
+  --
+  --
+  procedure Cli
+  is
+  begin
+    System.Machine_Code.Asm
+      (Template => "cli",
+       Volatile => True);
+  end Cli;
+
+  --
+  --
+  procedure CPUID
+    (EAX : in out Aperture.Word32;
+     EBX :    out Aperture.Word32;
+     ECX : in out Aperture.Word32;
+     EDX :    out Aperture.Word32)
+  is
+  begin
+    System.Machine_Code.Asm
+      (Template => "cpuid",
+       Inputs   => (Aperture.Word32'Asm_Input ("a", EAX),
+                    Aperture.Word32'Asm_Input ("c", ECX)),
+       Outputs  => (Aperture.Word32'Asm_Output ("=a", EAX),
+                    Aperture.Word32'Asm_Output ("=b", EBX),
+                    Aperture.Word32'Asm_Output ("=c", ECX),
+                    Aperture.Word32'Asm_Output ("=d", EDX)),
+       Volatile => True);
+  end CPUID;
+
+  --
+  --
+  procedure Halt
+  is
+  begin
+    System.Machine_Code.Asm
+      (Template => "hlt",
+       Volatile => True);
+  end Halt;
+
+  --
+  --
+  procedure Sti
+  is
+  begin
+     System.Machine_Code.Asm
+       (Template => "sti",
+        Volatile => True);
+  end Sti;
+
+  --
+  --
+  procedure Lidt (Address : Aperture.Word64)
+  is
+  begin
+     System.Machine_Code.Asm
+       (Template => "lidt (%0)",
+        Inputs   => (Aperture.Word64'Asm_Input ("r", Address)),
+        Volatile => True);
+  end Lidt;
+
+  --
+  --
+  procedure Panic
+  is
+  begin
+    System.Machine_Code.Asm
+      (Template => "ud2",
+       Volatile => True);
+  end Panic;
+
+  -------------------------------------------
+  -- The following (MSR) functions require the always_inline due to AMD
+  -- function STOP_CAR_AND_CPU that disables cache as
+  -- ram, the cache as ram stack can no longer be used. Called
+  -- functions must be inlined to avoid stack usage. Also, the
+  -- compiler must keep local variables register based and not
+  -- allocated them from the stack. With gcc 4.5.0, some functions
+  -- declared as inline are not being inlined. This patch forces
+  -- these functions to always be inlined by adding the qualifier
+  -- __attribute__((always_inline)) to their declaration.
+  -------------------------------------------
+
+  --
+  --
+  function Get_MSR64 (Register : Aperture.Word32) return Aperture.Word64
+  is
+     MSR : MSR_Type;
+  begin
+     Get_MSR (Register => Register,
+              MSR      => MSR);
+     return 2 ** 32 * Aperture.Word64 (MSR.High) + Aperture.Word64 (MSR.Low);
+  end Get_MSR64;
+
+  --
+  --
+  procedure Get_MSR
+    (Register :     Aperture.Word32;
+     MSR      : out MSR_Type)
+  is
+  begin
+     System.Machine_Code.Asm
+       (Template => "rdmsr",
+        Inputs   => (Aperture.Word32'Asm_Input ("c", Register)),
+        Outputs  => (Aperture.Word32'Asm_Output ("=d", MSR.High),
+                     Aperture.Word32'Asm_Output ("=a", MSR.Low)),
+        Volatile => True);
+  end Get_MSR;
+
+  --
+  --
+  procedure Write_MSR64
+    (Register : Aperture.Word32;
+     Value    : Aperture.Word64)
+  is
+     MSR : MSR_Type;
+  begin
+     MSR.Low  := Aperture.Word32'Mod (Value);
+     MSR.High := Aperture.Word32'Mod (Value / 2 ** 32);
+
+     Write_MSR (Register => Register,
+                MSR      => MSR);
+  end Write_MSR64;
+
+  --
+  --
+  procedure Write_MSR
+    (Register : Aperture.Word32;
+     MSR      : out MSR_Type)
+  is
+  begin
+     System.Machine_Code.Asm
+       (Template => "wrmsr",
+        Inputs   => (Aperture.Word32'Asm_Input ("a", MSR.Low),
+                     Aperture.Word32'Asm_Input ("d", MSR.High),
+                     Aperture.Word32'Asm_Input ("c", Register)),
+        Volatile => True);
+  end Write_MSR;
+
+  procedure Boot_CPU (BSP : out Boolean)
+  is
+    MSR : MSR_Type;
+  begin
+    Get_MSR (Register => 16#1b#, MSR => MSR);
+    BSP := (MSR.Low and 16#80#);
+  end Boot_CPU;
+
+end X86.CPU;
---
+++ src/arch/x86/x86-cpu.ads
@@ -0,0 +1,159 @@
+--
+-- Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
+--
+
+package X86.CPU is
+
+  --
+  -- CPU specific constants
+
+  -- Flags
+  RFLAGS_CF_FLAG               : constant := 0;
+  RFLAGS_ZF_FLAG               : constant := 6;
+  RFLAGS_IF_FLAG               : constant := 9;
+
+  CR0_PE_FLAG                  : constant := 0;
+  CR0_PG_FLAG                  : constant := 0;
+
+  CPUID_FEATURE_LOCAL_APIC     : constant := 9;
+  CPUID_FEATURE_X2APIC         : constant := 21;
+
+  -- MSR's
+  IA32_APIC_BASE               : constant := 16#1b#;
+  IA32_FEATURE_CONTROL         : constant := 16#3a#;
+  IA32_PMC0                    : constant := 16#c1#;
+  IA32_PMC1                    : constant := 16#c2#;
+  IA32_PMC2                    : constant := 16#c3#;
+  IA32_PMC3                    : constant := 16#c4#;
+  IA32_PMC4                    : constant := 16#c5#;
+  IA32_PMC5                    : constant := 16#c6#;
+  IA32_PMC6                    : constant := 16#c7#;
+  IA32_PMC7                    : constant := 16#c8#;
+  IA32_PERFEVTSEL0             : constant := 16#186#;
+  IA32_PERFEVTSEL1             : constant := 16#187#;
+  IA32_PERFEVTSEL2             : constant := 16#188#;
+  IA32_PERFEVTSEL3             : constant := 16#189#;
+  IA32_MISC_ENABLE             : constant := 16#1a0#;
+  IA32_EFER                    : constant := 16#c000_0080#;
+  IA32_KERNEL_GSBASE           : constant := 16#c000_0102#;
+
+  --
+  -- CPU specific registers.
+  type CPU_Registers_Type is
+    record
+      RAX : Word64;
+      RBX : Word64;
+      RCX : Word64;
+      RDX : Word64;
+      RDI : Word64;
+      RSI : Word64;
+      RBP : Word64;
+      R08 : Word64;
+      R09 : Word64;
+      R10 : Word64;
+      R11 : Word64;
+      R12 : Word64;
+      R13 : Word64;
+      R14 : Word64;
+      R15 : Word64;
+    end record;
+
+  Null_CPU_Regs : constant CPU_Registers_Type;
+
+  type MSR_Type is private;
+
+  -- Size of one page (4K)
+  Page_Size : constant := 4096;
+
+
+  --
+  -- Clear Interrupt Flag.
+  procedure Cli;
+  pragma Inline_Always (Cli);
+
+  --
+  -- Execute CPUID instruction.
+  procedure CPUID
+    (EAX : in out Aperture.Word32;
+     EBX :    out Aperture.Word32;
+     ECX : in out Aperture.Word32;
+     EDX :    out Aperture.Word32);
+  pragma Inline_Always (CPUID);
+
+  --
+  -- Halt the CPU.
+  procedure Halt;
+  pragma Inline_Always (Halt);
+
+  --  Set Interrupt Flag.
+  procedure Sti;
+  pragma Inline_Always (Sti);
+
+  --  Load Interrupt Descriptor Table (IDT) register.
+  procedure Lidt (Address : Aperture.Word64);
+  pragma Inline_Always (Lidt);
+
+  --
+  -- Panic.
+  procedure Panic;
+  pragma Inline_Always (Panic);
+
+  --
+  --  Return current value of given model specific register.
+  function Get_MSR64 (Register : Aperture.Word32) return Aperture.Word64;
+  pragma Inline_Always (Get_MSR64);
+
+  --
+  --  Return value of given MSR as low/high doublewords.
+  procedure Get_MSR
+    (Register :     Aperture.Word32;
+     MSR      : out MSR_Type);
+  pragma Inline_Always (Get_MSR);
+
+  --
+  --  Write specified quadword to given MSR.
+  procedure Write_MSR64
+    (Register : Aperture.Word32;
+     Value    : Aperture.Word64);
+  pragma Inline_Always (Write_MSR64);
+
+  --
+  --  Write specified low/high doublewords to given MSR.
+  procedure Write_MSR
+    (Register : Aperture.Word32;
+     MSR      : out MSR_Type);
+  pragma Inline_Always (Write_MSR);
+
+  --
+  --  ..
+  procedure Boot_CPU (BSP : out Boolean);
+  pragma Inline_Always (Boot_CPU);
+
+private
+
+  Null_CPU_Regs : constant CPU_Registers_Type := CPU_Registers_Type'
+    (RAX => 0,
+     RBX => 0,
+     RCX => 0,
+     RDX => 0,
+     RDI => 0,
+     RSI => 0,
+     RBP => 0,
+     R08 => 0,
+     R09 => 0,
+     R10 => 0,
+     R11 => 0,
+     R12 => 0,
+     R13 => 0,
+     R14 => 0,
+     R15 => 0);
+
+  --
+  --
+  type MSR_Type is
+    record
+      Low  : Aperture.Word32;
+      High : Aperture.Word32;
+    end record;
+
+end X86.CPU;
