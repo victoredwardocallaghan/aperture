75321213bbea077f4e277249bad237dbd9732ce8
Edward O'Callaghan <eocallaghan@alterapraxis.com>
2014-07-23 12:05:35 +1000

    Hook up a basic build system
    This needs a lot more work..
    
    Signed-off-by: Edward O'Callaghan <eocallaghan@alterapraxis.com>

--- src/cpu/car_main_entry.adb
+++ src/cpu/car_main_entry.adb
@@ -1,5 +1,5 @@
 --
--- Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
+--  Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
 --
 
 with Aperture.Text_IO;
@@ -7,24 +7,25 @@ with Aperture.Version;
 
 package body CAR_Main_Entry is
 
-  package Debug renames Aperture.Text_IO;
+   package Debug renames Aperture.Text_IO;
 
   --
-  -- This is called by cache_as_ram.inc as the chosen entry point from CAR !!!
-  procedure Cache_As_RAM_Main (Bist               : in Aperture.Word32;
-                               CPU_Init_DetectEDX : in Aperture.Word32)
-  is
-  begin
-    Debug.Put_Line (Item => "Booting *** Aperture *** firmware "
-                   & Aperture.Version.Version_String & " ("
-                   & Standard'Compiler_Version & ").");
+  --  This is called by cache_as_ram.inc as the chosen entry point from CAR !!!
+   procedure Cache_As_RAM_Main (Bist               : Aperture.Word32;
+                                CPU_Init_DetectEDX : Aperture.Word32) is
+   begin
+      Debug.Put_Line (Item => "Booting *** Aperture *** firmware "
+                     & Aperture.Version.Version_String & " ("
+                     & Standard'Compiler_Version & ").");
 
     --
-    -- Do stuff???
+    --  Do stuff???
 
-    Debug.Put_Line (Item => "<> CPU_Init_DetectEDX = "
-                   & Aperture.Word32'Image (CPU_Init_DetectEDX));
+      Debug.Put_Line (Item => "<*> Bist = " & Aperture.Word32'Image (Bist));
 
-  end Cache_As_RAM_Main;
+      Debug.Put_Line (Item => "<> CPU_Init_DetectEDX = "
+                     & Aperture.Word32'Image (CPU_Init_DetectEDX));
+
+   end Cache_As_RAM_Main;
 
 end CAR_Main_Entry;
--- src/cpu/car_main_entry.ads
+++ src/cpu/car_main_entry.ads
@@ -1,14 +1,17 @@
 --
--- Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
+--  Copyright (C) 2014 Edward O'Callaghan <eocallaghan@alterapraxis.com>
 --
+
+with Aperture;
 
 package CAR_Main_Entry is
 
   --
-  -- This is called by cache_as_ram.inc as the chosen entry point from CAR !!!
-  procedure Cache_As_RAM_Main (Bist               : in Aperture.Word32;
-                               CPU_Init_DetectEDX : in Aperture.Word32);
-  --
-  pragma Export (C, CAR_Main_Entry, "cache_as_ram_main");
+  --  This is called by cache_as_ram.inc as the chosen entry point from CAR !!!
+   procedure Cache_As_RAM_Main (Bist               : Aperture.Word32;
+                                CPU_Init_DetectEDX : Aperture.Word32);
+   --
+   pragma Export (C, Cache_As_RAM_Main, "cache_as_ram_main");
+   pragma Inline_Always (Cache_As_RAM_Main);
 
 end CAR_Main_Entry;
